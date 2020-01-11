5672b357938bbefbc761a15aaf089ef3a8bec22b
Shokry <shokry92@gmail.com>
2020-01-11 20:43:58 +0100

    Add socket context

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { ThemeProvider } from "styled-components";
+import io from "socket.io-client";
 import MovieList from "./views/MovieList";
 import Player from "./views/Player";
+import socketContext from "./contexts/socket";
+import { SOCKET_URL } from "./constants";
 
 const theme = {
   colors: {
@@ -10,6 +13,12 @@ const theme = {
     backgroundSecondary: "#2D1C26",
     primary: "#987284",
     secondary: "#6C534E"
+  },
+  spacing: {
+    xSmall: 5,
+    small: 10,
+    medium: 15,
+    large: 32
   }
 };
 
@@ -17,17 +26,19 @@ function App() {
   return (
     <Router>
       <ThemeProvider theme={theme}>
-        <Switch>
-          <Route exact path="/player">
-            <Player />
-          </Route>
-          <Route path="/movielist">
-            <MovieList />
-          </Route>
-          <Route>
-            <p>How did you get in here? XD</p>
-          </Route>
-        </Switch>
+        <socketContext.Provider value={io(SOCKET_URL)}>
+          <Switch>
+            <Route exact path="/player">
+              <Player />
+            </Route>
+            <Route path="/movielist">
+              <MovieList />
+            </Route>
+            <Route>
+              <p>How did you get in here? XD</p>
+            </Route>
+          </Switch>
+        </socketContext.Provider>
       </ThemeProvider>
     </Router>
   );
--- src/views/MovieList/components/MovieDetails.js
+++ src/views/MovieList/components/MovieDetails.js
@@ -1,11 +1,15 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useContext } from "react";
 import styled from "styled-components";
 import { useParams, useHistory } from "react-router-dom";
+import socketContext from "../../../contexts/socket";
+import { ArrowLeftCircle } from "react-feather";
 
 function MovieDetails(props) {
-  const { movies } = props;
   const { movieId } = useParams();
   const history = useHistory();
+  const socket = useContext(socketContext);
+
+  const { movies } = props;
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -15,12 +19,28 @@ function MovieDetails(props) {
     return null;
   }
 
-  const { title, description, poster } = movies[movieId];
+  const { title, description, poster, trailerUrl, movieUrl } = movies[movieId];
   return (
     <Container>
-      <BackButton onClick={() => history.goBack()}>Back to list</BackButton>
+      <BackButton onClick={() => history.goBack()}>
+        <ArrowLeftCircle size="32" />
+      </BackButton>
       <Poster src={poster} alt={`${title} poster`} />
       <Title>{title}</Title>
+      <WatchButtonsContainer>
+        <button
+          onClick={() =>
+            socket.emit("watch/trailer", { title, url: trailerUrl })
+          }
+        >
+          Watch trailer
+        </button>
+        <button
+          onClick={() => socket.emit("watch/movie", { title, url: movieUrl })}
+        >
+          Watch movie
+        </button>
+      </WatchButtonsContainer>
       <Title>{description}</Title>
     </Container>
   );
@@ -41,7 +61,7 @@ const Poster = styled.img`
   object-fit: cover;
 `;
 
-const Title = styled.h1`
+const Title = styled.p`
   text-align: center;
   font-size: 32px;
   color: ${props => props.theme.colors.primary};
@@ -49,8 +69,16 @@ const Title = styled.h1`
   margin-top: 0;
 `;
 
+const WatchButtonsContainer = styled.div`
+  display: flex;
+`;
+
 const BackButton = styled(Title)`
   cursor: pointer;
+  align-self: start;
+  margin-left: ${props => props.theme.spacing.small}px;
+  margin-bottom: ${props => props.theme.spacing.small}px;
+  margin-top: ${props => props.theme.spacing.small}px;
 `;
 
 export default MovieDetails;
